958ef6968e5d3c81ae9a1ceb7ebbee980d3dbe35
mhmtwebdev <mahmutisiz@gmail.com>
2026-01-05 17:50:00 +0300

    feat: Configure Vite to manually chunk vendor dependencies and set chunk size warning limit.

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/libratech1/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'firebase/app', 'firebase/firestore', 'html5-qrcode'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  }
 });
